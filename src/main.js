285c106ce5a8207f050da56f6c1d653590c2bb7f
Marcos Ferreira <marcos.ferreira@hlink.pt>
2022-09-30 17:52:52 +0100

    app

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from "vue";
+import { Chrome, create } from "@ckpack/vue-color";
+import App from "./App.vue";
+
+const files = require.context("./", true, /\.vue$/i);
+files
+  .keys()
+  .map((key) =>
+    App.component(key.split("/").pop().split(".")[0], files(key).default)
+  );
+
+createApp(App)
+  .use(
+    create({
+      components: [Chrome],
+    })
+  )
+  .mount("#app");
